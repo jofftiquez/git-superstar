57fb199fec135919d6291dfba5b8a2ddbbd00ffa
Joff Tiquez <jofftiquez@gmail.com>
2018-12-17 01:23:32 +0800

    Update font

--- src/app.vue
+++ src/app.vue
@@ -4,9 +4,9 @@
     v-footer(fixed height="auto").pa-2
       v-layout(row)
         v-flex
-          <a class="github-button mt-1" href="https://github.com/jofftiquez" data-size="large" data-show-count="true" aria-label="Follow @jofftiquez on GitHub">Follow @jofftiquez</a>
-          <a class="github-button mt-1" href="https://github.com/jofftiquez/git-superstar" data-size="large" data-show-count="true" aria-label="Star jofftiquez/git-superstar on GitHub">Star</a>
-          strong.mt-1.right Made with ❤️by 
+          //- <a class="github-button mt-1" href="https://github.com/jofftiquez" data-size="large" data-show-count="true" aria-label="Follow @jofftiquez on GitHub">Follow @jofftiquez</a>
+          //- <a class="github-button mt-1" href="https://github.com/jofftiquez/git-superstar" data-size="large" data-show-count="true" aria-label="Star jofftiquez/git-superstar on GitHub">Star</a>
+          strong.right Made with ❤️by 
             a(href="https://twitter.com/jrtiquez" target="_blank") @jrtiquez
 </template>
 
@@ -28,9 +28,7 @@ export default {
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  font-family: 'Nanum Gothic', sans-serif;
   color: #2c3e50;
 }
 </style>
