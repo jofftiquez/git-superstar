c96615f8f87f9ea09a537f6e817ff76ffa942f68
Jofferson R Tiquez <jofftiquez@gmail.com>
2019-07-21 12:42:43 +0800

    refactor: update app.vue

--- src/app.vue
+++ src/app.vue
@@ -6,20 +6,20 @@
         v-flex
           //- <a class="github-button mt-1" href="https://github.com/jofftiquez" data-size="large" data-show-count="true" aria-label="Follow @jofftiquez on GitHub">Follow @jofftiquez</a>
           //- <a class="github-button mt-1" href="https://github.com/jofftiquez/git-superstar" data-size="large" data-show-count="true" aria-label="Star jofftiquez/git-superstar on GitHub">Star</a>
-          strong.right Made with ❤️by 
+          strong.right Made with ❤️ by 
             a(href="https://twitter.com/jrtiquez" target="_blank") @jrtiquez
 </template>
 
 <script>
 export default {
   name: 'app',
-  data() {
+  data () {
     return {
       theme: 'dark'
     }
   },
   methods: {
-    getTheme() {
+    getTheme () {
       return this.theme === 'dark' ? true : false;
     }
   }
